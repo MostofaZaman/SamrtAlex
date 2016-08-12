eccc905293c99bf0ac81a856eac9e4331226ef6f
MostofaZaman <mostofa_sub@yahoo.com>
2016-08-12 07:31:58 +0600

    Add files via upload

---
+++ Infrastructure/DbFactory.cs
@@ -0,0 +1,26 @@
+﻿using MVCArch.Data.Infrastructure;
+using MVCArch.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCArch.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        MVCArchContext dbContext;
+
+        public MVCArchContext Init()
+        {
+            return dbContext ?? (dbContext = new MVCArchContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
