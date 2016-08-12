409f409c6c637badcf15ae4c07259557f5322559
MostofaZaman <mostofa_sub@yahoo.com>
2016-08-12 07:32:42 +0600

    Add files via upload

---
+++ MVCArchEntities.cs
@@ -0,0 +1,23 @@
+﻿using MVCArch.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCArch.Data
+{
+    public class MVCArchContext : DbContext
+    {
+        public MVCArchContext() : base("MVCArchContext") { }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        public virtual void Commit()
+        {
+            base.SaveChanges();
+        }
+
+    }
+}
---
+++ MVCArchSeedData.cs
@@ -0,0 +1,37 @@
+﻿using MVCArch.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCArch.Data
+{
+    public class MVCArchSeedData : DropCreateDatabaseIfModelChanges<MVCArchContext>
+    {
+        protected override void Seed(MVCArchContext context)
+        {
+            GetEmployees().ForEach(e => context.Employees.Add(e));
+
+            context.Commit();
+        }
+
+        private static List<Employee> GetEmployees()
+        {
+            return new List<Employee>
+            {
+                new Employee {
+                    Name = "Mostofa"
+                },
+                new Employee {
+                    Name = "Zaman"
+                },
+                new Employee {
+                    Name = "Mike"
+                }
+            };
+        }
+   
+    }
+}
